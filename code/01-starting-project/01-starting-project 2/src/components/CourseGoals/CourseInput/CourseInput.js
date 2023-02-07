d2f3a4f7143c3d4623575d6ae8cfca82fa871b58
John Law <johnlaw114@gmail.com>
2023-02-07 17:00:12 -0500

    first of reviewing concepts and tech

---
+++ code/01-starting-project/01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+import Button from '../../UI/Button/Button';
+import styles from './CourseInput.module.css';
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     //holy shit you can pass props with jsx with these styled comp!
+//     color: ${(props) => (props.invalid ? 'red' : 'black')};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+//     background: ${(props) => (props.invalid ? '$ffd7d7' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+    setIsValid(true);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      {/* Below one way just triggering the invalid class */}
+      {/* <FormControl className={!isValid && 'invalid'}> */}
+      {/* [FYI]Another way is to pass the usestate as prop to trigger the classname */}
+      //this is how you add styles with css modules
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+        <label>Course Goal</label>
+        <input type='text' onChange={goalInputChangeHandler} />
+      </div>
+      <Button type='submit'>Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
