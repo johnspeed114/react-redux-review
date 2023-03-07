f45373fb48d3cf3d09ac059d106d0920f016dc8a
John Law <johnlaw114@gmail.com>
2023-03-07 16:02:23 -0500

    refactor request http with custom reusable hooks

--- code/04-onwards-to-a-more-realistic-example/src/App.js
+++ code/04-onwards-to-a-more-realistic-example/src/App.js
@@ -2,42 +2,32 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  //just need usecallback here since this function obj is outside of sendTasks function(dependencies)
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-2458e-default-rtdb.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  //in destructuring you can change the name with a colon after the key
+  const { loading, error, sendTasks: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (taskObj) => {
+      const loadedTask = [];
+      for (const taskKey in taskObj) {
+        loadedTask.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTask);
+    };
+    fetchTasks(
+      {
+        url: 'https://react-http-2458e-default-rtdb.firebaseio.com/tasks.json',
+      },
+      transformTasks
+    );
+    //we want to check the fetchtasks is updated in the dependency but to do that we need usecallback, otherwisee, we get infinite loop
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
@@ -48,7 +38,7 @@ function App() {
       <NewTask onAddTask={taskAddHandler} />
       <Tasks
         items={tasks}
-        loading={isLoading}
+        loading={loading}
         error={error}
         onFetch={fetchTasks}
       />
--- code/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ code/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -1,46 +1,35 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
+import useHttp from '../../hooks/use-http';
+
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { loading, error, sendTasks: sendTaskRequest } = useHttp();
+
+  const transformNewTask = (taskText, data) => {
+    const generatedId = data.name;
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-2458e-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: 'https://react-http-2458e-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: { text: taskText },
+      },
+      //bind method preconfig function before calling, js method default
+      //first arg sets the 'this' of the obj, but we don't need it here
+      //[fyi]all data already defined in the parameter will be appended afterwards
+      transformNewTask.bind(null, taskText)
+    );
   };
 
   return (
     <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      <TaskForm onEnterTask={enterTaskHandler} loading={loading} />
       {error && <p>{error}</p>}
     </Section>
   );
