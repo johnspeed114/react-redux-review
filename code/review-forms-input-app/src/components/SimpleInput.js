473b19b431f020ce07baa4eb7e8a694cb6097446
John Law <johnlaw114@gmail.com>
2023-03-08 20:20:46 -0500

    usestate set up

--- code/review-forms-input-app/src/components/SimpleInput.js
+++ code/review-forms-input-app/src/components/SimpleInput.js
@@ -1,11 +1,15 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  //we will use state first and set them to listen to input's onchanges
+  const [enteredName, setEnteredName] = useState('');
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
